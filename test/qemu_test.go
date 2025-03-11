8c87f7a87335ce95c33dfaeb45de33d1300e88dd
Alessio Biancalana <alessio@dottorblaster.it>
2025-03-11 19:53:22 +0100

    Add a first test using QEMU

---
+++ test/qemu_test.go
@@ -0,0 +1,88 @@
+package tests
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/anatol/vmtest"
+	"github.com/tmc/scp"
+	"golang.org/x/crypto/ssh"
+)
+
+func TestBootInQemu(t *testing.T) {
+	// qemu_run_test.go is your test file from above. We have to compile it as a binary and copy to a QEMU vm
+	cmd := exec.Command("go", "test", "-c", "../internal/containerd/containerd_test.go", "-o", "qemu_run_test")
+	if testing.Verbose() {
+		log.Print("compile in-qemu test binary")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+	err := cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove("qemu_run_test")
+
+	// These integration tests use QEMU with a statically-compiled kernel (to avoid inintramfs) and specially
+	// prepared rootfs. See TESTING.md.md file for instructions how to prepare
+	opts := vmtest.QemuOptions{
+		OperatingSystem: vmtest.OS_LINUX,
+		Kernel:          "../testdata/bzImage",
+		Params:          []string{"-net", "user,hostfwd=tcp::10022-:22", "-net", "nic", "-enable-kvm", "-cpu", "host", "-m", "2048M"},
+		Disks:           []vmtest.QemuDisk{vmtest.QemuDisk{Path: "../testdata/rootfs.cow", Format: "qcow2"}},
+		Append:          []string{"root=/dev/sda", "rw"},
+		Verbose:         testing.Verbose(),
+		Timeout:         50 * time.Second,
+	}
+	// Run QEMU instance
+	qemu, err := vmtest.NewQemu(&opts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Shutdown QEMU at the end of the test case
+	defer qemu.Shutdown()
+
+	config := &ssh.ClientConfig{
+		User:            "root",
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	conn, err := ssh.Dial("tcp", "localhost:10022", config)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	sess, err := conn.NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer sess.Close()
+
+	scpSess, err := conn.NewSession()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = scp.CopyPath("qemu_run_test", "qemu_run_test", scpSess)
+	if err != nil {
+		t.Error(err)
+	}
+
+	testCmd := "./qemu_run_test"
+	if testing.Verbose() {
+		testCmd += " -test.v"
+	}
+
+	output, err := sess.CombinedOutput(testCmd)
+	if testing.Verbose() {
+		fmt.Print(string(output))
+	}
+	if err != nil {
+		t.Fatal(err)
+	}
+}
