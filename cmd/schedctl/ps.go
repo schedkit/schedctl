b79a3f89b23cfa069d2c32f97cb1f033e38d3b2f
Alessio Biancalana <alessio@dottorblaster.it>
2025-04-25 13:35:14 +0200

    feat: add multi-driver support

--- cmd/schedctl/ps.go
+++ cmd/schedctl/ps.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"schedctl/internal/containerd"
+	"schedctl/internal/containers"
 	"schedctl/internal/output"
 )
 
@@ -14,19 +15,32 @@ func NewPsCmd() *cobra.Command {
 		RunE:  ps,
 	}
 
+	psCmd.PersistentFlags().StringP("driver", "d", "containerd", "The driver to use: containerd, podman")
+
 	return psCmd
 }
 
-func ps(_ *cobra.Command, _ []string) error {
+func ps(cmd *cobra.Command, _ []string) error {
+	driver := cmd.Flags().Lookup("driver").Value.String()
 	client, err := containerd.NewClient()
 	if err != nil {
 		panic(err)
 	}
 	defer client.Close()
 
-	containersList, err := containerd.List(client)
-	if err != nil {
-		panic(err)
+	containersList := make([]containers.Container, 0)
+
+	if driver == "containerd" {
+		containerdList, err := containerd.List(client)
+		if err != nil {
+			panic(err)
+		}
+		for _, container := range containerdList {
+			containersList = append(containersList, container)
+		}
+	}
+
+	if driver == "podman" {
 	}
 
 	for _, container := range containersList {
