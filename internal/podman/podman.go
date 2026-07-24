fa1cdf1a146d537e33c3f97de330b24963281c94
Alessio Biancalana <alessio@dottorblaster.it>
2026-07-24 19:43:40 +0200

    build(deps): upgrade containerd to v2.3.3 and podman to v6

--- internal/podman/podman.go
+++ internal/podman/podman.go
@@ -9,10 +9,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/containers/podman/v5/pkg/bindings"
-	podman_containers "github.com/containers/podman/v5/pkg/bindings/containers"
-	"github.com/containers/podman/v5/pkg/bindings/images"
-	"github.com/containers/podman/v5/pkg/specgen"
+	"go.podman.io/podman/v6/pkg/bindings"
+	podman_containers "go.podman.io/podman/v6/pkg/bindings/containers"
+	"go.podman.io/podman/v6/pkg/bindings/images"
+	"go.podman.io/podman/v6/pkg/specgen"
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 
 	"schedctl/internal/containers"
